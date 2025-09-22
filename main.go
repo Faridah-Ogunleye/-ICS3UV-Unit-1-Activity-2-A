4b45e22aec988e0071d6dcadf5c3de65a2d076aa
default <15580ogu@wcdsbcloud.ca>
2025-09-22 22:58:53 +0000

    what you have changed since last commit

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ * Author: Faridah Ogunleye
+ * Version: 1.0.0
+ *Date: 2025-09-22
+ * This is the traditional "Hello, World!" program.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// print out the greeting
+		fmt.Println("Hello, World!")
+
+		fmt.Println("\nDone.")
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Faridah Ogunleye
+ * @version 1.0.0
+ * @date 2025-09-22
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone.");
